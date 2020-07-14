a8c24d125d3dc2da8a34fc089d25134512e37c46
Ales Kancilija <ales.kancilija@hotmail.com>
2020-07-14 22:44:39 +0200

    Add TimeularException for all other errors

--- timeular/exceptions.py
+++ timeular/exceptions.py
@@ -1,4 +1,3 @@
-
 class TimeularHttpException(Exception):
 
     def __init__(self, status_code: int, url: str, message: str = None) -> None:
@@ -14,3 +13,18 @@ class TimeularHttpException(Exception):
 
     def __repr__(self):
         return f'<TimeularHttpException [{self.status_code}]>'
+
+
+class TimeularException(Exception):
+
+    def __init__(self, message: str = None) -> None:
+        """TimeularException raised for all other errors
+        """
+        super().__init__()
+        self.message = message or ''
+
+    def __str__(self):
+        return f'TimeularException: {self.message}'
+
+    def __repr__(self):
+        return f'<TimeularException [{self.message}]>'
